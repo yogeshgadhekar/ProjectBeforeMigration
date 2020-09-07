b172401d3245edf93321aa861cda8db67e7834ba
yogeshgadhekar <yogesh_gadhekar@persistent.com>
2020-09-07 16:28:26 +0530

    initial commit

---
+++ MigraionDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigraionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is before migratio to .net core");
+            Console.Read();
+        }
+    }
+}
